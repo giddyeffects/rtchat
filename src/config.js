fc7132423b13b39b14a3c1e510a285a6d855c596
Nikiman <nikimangroup@gmail.com>
2017-12-21 12:53:18 +0300

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+  // Firebase config
+  const config = {
+    apiKey: "AIzaSyAdorKj710OHV8pd4jv9VSVP5ISYm07UFc",
+    authDomain: "real-time-a9e43.firebaseapp.com",
+    databaseURL: "https://real-time-a9e43.firebaseio.com",
+    projectId: "real-time-a9e43",
+    storageBucket: "",
+    messagingSenderId: "1001457236435"
+  };
+
+  export default config
